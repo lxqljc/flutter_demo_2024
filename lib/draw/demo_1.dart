9c1113a9cfd337435f3610fa6c7a18ba2ffb0dfd
luoxiaoquan <luoxiaoquan@flyele.net>
2024-08-13 15:56:25 +0800

    feat: demo1

---
+++ lib/draw/demo_1.dart
@@ -0,0 +1,55 @@
+/// 作者: xiaoquanluo
+/// 日期: 2024/8/13
+/// 描述: 绘制圆形
+
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // 确定初始化
+  // SystemChrome.setPreferredOrientations(// 使设备横屏显示
+  //     [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive,
+  //     overlays: []); // 全屏显示
+
+  runApp(
+    const MaterialApp(
+      home: Paper(),
+    ),
+  );
+}
+
+/// 纸张
+class Paper extends StatelessWidget {
+  const Paper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: CustomPaint(
+        painter: PaperPainter(context),
+      ),
+    );
+  }
+}
+
+class PaperPainter extends CustomPainter {
+  PaperPainter(this.context);
+
+  BuildContext context;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // 创建画笔
+    final Paint paint = Paint();
+    // 绘制圆形
+    final double top = MediaQuery.of(context).padding.top;
+    canvas.drawCircle(Offset(100, top + 10), 10, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
