d51ca8550ea24bd8eea82e08e9661ee1678e4e89
luoxiaoquan <luoxiaoquan@flyele.net>
2024-08-15 15:23:43 +0800

    feat: 更新 demo5

---
+++ lib/draw/demo_5.dart
@@ -0,0 +1,63 @@
+/// 作者: xiaoquanluo
+/// 日期: 2024/8/13
+/// 描述: 绘制圆形
+
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // 确定初始化
+  // SystemChrome.setPreferredOrientations(// 使设备横屏显示
+  //     [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive,
+  //     overlays: []); // 全屏显示
+
+  runApp(
+    const MaterialApp(
+      home: Paper(),
+    ),
+  );
+}
+
+/// 纸张
+class Paper extends StatelessWidget {
+  const Paper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: CustomPaint(
+        painter: PaperPainter(context),
+      ),
+    );
+  }
+}
+
+class PaperPainter extends CustomPainter {
+  PaperPainter(this.context);
+
+  BuildContext context;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    Paint paint = Paint();
+    paint
+      ..style = PaintingStyle.stroke
+      ..color = Colors.blue
+      ..strokeWidth = 20;
+    
+    canvas.drawLine(const Offset(50, 50), const Offset(50, 150), paint..strokeCap = StrokeCap.butt);
+
+    canvas.drawLine(const Offset(100, 50), const Offset(100, 150), paint..strokeCap = StrokeCap.round);
+
+    canvas.drawLine(const Offset(150, 50), const Offset(150, 150), paint..strokeCap = StrokeCap.square);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
