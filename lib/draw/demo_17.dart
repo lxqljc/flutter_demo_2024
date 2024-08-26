63dcfc94d80f7e5f15b6e83cce8a75ea0f851b69
luoxiaoquan <luoxiaoquan@flyele.net>
2024-08-26 18:56:10 +0800

    feat: 更新 demo17

---
+++ lib/draw/demo_17.dart
@@ -0,0 +1,83 @@
+/// 作者: xiaoquanluo
+/// 日期: 2024/8/13
+/// 描述: 绘制圆形
+
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // 确定初始化
+  // SystemChrome.setPreferredOrientations(// 使设备横屏显示
+  //     [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive,
+  //     overlays: []); // 全屏显示
+
+  runApp(
+    const MaterialApp(
+      home: Paper(),
+    ),
+  );
+}
+
+/// 纸张
+class Paper extends StatefulWidget {
+  const Paper({super.key});
+
+  @override
+  State<Paper> createState() => _PaperState();
+}
+
+class _PaperState extends State<Paper> {
+  ui.Image? _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return _image == null
+        ? Container()
+        : Container(
+            color: Colors.white,
+            child: CustomPaint(
+              painter: PaperPainter(context, _image!),
+            ),
+          );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadImageFromAssets();
+  }
+
+  Future<void> loadImageFromAssets() async {
+    final ByteData data = await rootBundle.load('assets/images/animal.png');
+    final Uint8List bytes = data.buffer.asUint8List();
+
+    ui.decodeImageFromList(bytes, (ui.Image img) {
+      setState(() {
+        _image = img;
+      });
+    });
+  }
+}
+
+class PaperPainter extends CustomPainter {
+  PaperPainter(this.context, this.image);
+
+  BuildContext context;
+
+  final ui.Image image;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // 将图像绘制到画布上
+    canvas.translate((size.width - image.width) / 2, (size.height - image.height) / 2);
+    canvas.drawImage(image, Offset.zero, Paint());
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
