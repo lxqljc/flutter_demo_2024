51f4dbad4b131b3bad332292ba04b0deaf926510
luoxiaoquan <luoxiaoquan@flyele.net>
2024-08-27 14:42:49 +0800

    feat: 更新 demo24

---
+++ lib/draw/demo_24.dart
@@ -0,0 +1,69 @@
+/// 作者: xiaoquanluo
+/// 日期: 2024/8/13
+/// 描述: 绘制圆形
+
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // 确定初始化
+  // SystemChrome.setPreferredOrientations(// 使设备横屏显示
+  //     [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive,
+  //     overlays: []); // 全屏显示
+
+  runApp(
+    const MaterialApp(
+      home: Paper(),
+    ),
+  );
+}
+
+/// 纸张
+class Paper extends StatelessWidget {
+  const Paper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: CustomPaint(
+        painter: PaperPainter(context),
+      ),
+    );
+  }
+}
+
+class PaperPainter extends CustomPainter {
+  PaperPainter(this.context);
+
+  BuildContext context;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.translate(size.width / 2, size.height / 2);
+    Path path = Path();
+    Paint paint = Paint()
+      ..color = Colors.purpleAccent
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke;
+    path.lineTo(80, -40);
+
+    //绘制中间
+    path
+      ..arcToPoint(
+        const Offset(40, 40),
+        radius: const Radius.circular(100),
+        largeArc: false,
+      )
+      ..close();
+    canvas.drawPath(path, paint);
+
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
